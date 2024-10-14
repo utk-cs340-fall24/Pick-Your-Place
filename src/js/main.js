2abf984c7b25a411e4ba28d538751d5fcfa592cb
Trinity Sanaa Bissahoyo <tbissaho@tesla21.eecs.utk.edu>
2024-10-14 01:35:48 -0400

    added search bars and alerts

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let alert_box = document.getElementById('alert_box');
+
+function added_alert(msg){
+    let alert = document.createElement('div');
+    alert.classList.add('alert');
+    alert.innerHTML = msg;
+    alert_box.appendChild(alert);
+
+    if (msg.includes('Removed')) {
+        alert.classList.add('remove');
+    }
+
+
+    setTimeout(() => {
+      alert.remove();
+    },3000);
+
+    
+}
+    
     //Provides the Add and Remove button for the entire city
     const addRemoveCityButtons = document.querySelectorAll('.add-remove-city');
 
@@ -8,10 +28,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 button.classList.remove("btn-outline-success");
                 button.classList.add("btn-outline-danger");
                 button.textContent = "Remove City from Bucket List";
+                added_alert("City Added!");
             } else {
                 button.classList.remove("btn-outline-danger");
                 button.classList.add("btn-outline-success");
                 button.textContent = "Add City to Bucket List";
+                added_alert('City Removed');
             }
         });
     })
@@ -25,10 +47,28 @@ document.addEventListener('DOMContentLoaded', function () {
                 button.classList.remove("btn-outline-success");
                 button.classList.add("btn-outline-danger");
                 button.textContent = "Remove";
+                
+                let type = button.getAttribute('data-type');
+                if (type == 'spot') {
+                    added_alert("Attraction Added");
+                } else if (type == 'eats') {
+                    added_alert("Food Place Added");
+                } else if (type == 'lodging') {
+                    added_alert("Lodging Added");
+                }
             } else {
                 button.classList.remove("btn-outline-danger");
                 button.classList.add("btn-outline-success");
                 button.textContent = "Add";
+
+                let type = button.getAttribute('data-type');
+                if (type == 'spot') {
+                    added_alert("Attraction Removed");
+                } else if (type == 'eats') {
+                    added_alert("Food Place Removed");
+                } else if (type == 'lodging') {
+                    added_alert("Lodging Removed");
+                }
             }
         });
     });
