fb74bfb7474830b5f984fa5fe737e6f2062cd077
Nia M <unikorn.nkm31@gmail.com>
2024-10-27 22:17:32 -0400

    added blurbs for Kyoto tourist stuff. Adding tokyo blurbs tomorrow.

--- src/js/data.js
+++ src/js/data.js
@@ -3,439 +3,456 @@
  * Source: https://worldpopulationreview.com
  */
 export const allCities = [
-    {
-        name: "Buenos Aires",
-        country: "Argentina",
-        population: "15,618,288",
-    },
-    {
-        name: "Mendoza",
-        country: "Argentina",
-        population: "1,242,319",
-    },
-    {
-        name: "Colmar",
-        country: "France",
-        population: "71,163",
-    },
-    {
-        name: "Paris",
-        country: "France",
-        population: "11,276,701",
-    },
-    {
-        name: "Kyoto",
-        country: "Japan",
-        population: "1,459,640",
-    },
-    {
-        name: "Tokyo",
-        country: "Japan",
-        population: "8,336,599",
-    },
-    {
-        name: "Casablanca",
-        country: "Morocco",
-        population: "3,950,408",
-    },
-    {
-        name: "Rabat",
-        country: "Morocco",
-        population: "1,989,197",
-    },
-    {
-        name: "Los Angeles",
-        country: "US",
-        population: "3,795,936",
-        state: "California",
-    },
-    {
-        name: "New York",
-        country: "US",
-        population: "8,097,282",
-        state: "New York",
-    },
+  {
+    name: "Buenos Aires",
+    country: "Argentina",
+    population: "15,618,288",
+  },
+  {
+    name: "Mendoza",
+    country: "Argentina",
+    population: "1,242,319",
+  },
+  {
+    name: "Colmar",
+    country: "France",
+    population: "71,163",
+  },
+  {
+    name: "Paris",
+    country: "France",
+    population: "11,276,701",
+  },
+  {
+    name: "Kyoto",
+    country: "Japan",
+    population: "1,459,640",
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    population: "8,336,599",
+  },
+  {
+    name: "Casablanca",
+    country: "Morocco",
+    population: "3,950,408",
+  },
+  {
+    name: "Rabat",
+    country: "Morocco",
+    population: "1,989,197",
+  },
+  {
+    name: "Los Angeles",
+    country: "US",
+    population: "3,795,936",
+    state: "California",
+  },
+  {
+    name: "New York",
+    country: "US",
+    population: "8,097,282",
+    state: "New York",
+  },
 ];
 
 /**
  * To import: import { argentinaCities } from "./data.js";
  */
 export const argentinaCities = [
-    {
-        name: "Buenos Aires",
-        country: "Argentina",
-        population: "15,618,288",
-        top_spots: [
-            {
-                name: "BA-TS1",
-                blurb: "BA-TS1 blurb",
-            },
-            {
-                name: "BA-TS2",
-                blurb: "BA-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "BA-PE1",
-                blurb: "BA-PE1 blurb",
-            },
-            {
-                name: "BA-PE2",
-                blurb: "BA-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "BA-L1",
-                blurb: "BA-L1 blurb",
-            },
-            {
-                name: "BA-L2",
-                blurb: "BA-L2 blurb",
-            },
-        ],
-    },
-    {
-        name: "Mendoza",
-        country: "Argentina",
-        population: "1,242,319",
-        top_spots: [
-            {
-                name: "M-TS1",
-                blurb: "M-TS1 blurb",
-            },
-            {
-                name: "M-TS2",
-                blurb: "M-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "M-PE1",
-                blurb: "M-PE1 blurb",
-            },
-            {
-                name: "M-PE2",
-                blurb: "M-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "M-L1",
-                blurb: "M-L1 blurb",
-            },
-            {
-                name: "M-L2",
-                blurb: "M-L2 blurb",
-            },
-        ],
-    },
+  {
+    name: "Buenos Aires",
+    country: "Argentina",
+    population: "15,618,288",
+    top_spots: [
+      {
+        name: "BA-TS1",
+        blurb: "BA-TS1 blurb",
+      },
+      {
+        name: "BA-TS2",
+        blurb: "BA-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "BA-PE1",
+        blurb: "BA-PE1 blurb",
+      },
+      {
+        name: "BA-PE2",
+        blurb: "BA-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "BA-L1",
+        blurb: "BA-L1 blurb",
+      },
+      {
+        name: "BA-L2",
+        blurb: "BA-L2 blurb",
+      },
+    ],
+  },
+  {
+    name: "Mendoza",
+    country: "Argentina",
+    population: "1,242,319",
+    top_spots: [
+      {
+        name: "M-TS1",
+        blurb: "M-TS1 blurb",
+      },
+      {
+        name: "M-TS2",
+        blurb: "M-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "M-PE1",
+        blurb: "M-PE1 blurb",
+      },
+      {
+        name: "M-PE2",
+        blurb: "M-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "M-L1",
+        blurb: "M-L1 blurb",
+      },
+      {
+        name: "M-L2",
+        blurb: "M-L2 blurb",
+      },
+    ],
+  },
 ];
 
 /**
  * To import: import { franceCities } from "./data.js";
  */
 export const franceCities = [
-    {
-        name: "Colmar",
-        country: "France",
-        population: "71,163",
-        top_spots: [
-            {
-                name: "F-TS1",
-                blurb: "F-TS1 blurb",
-            },
-            {
-                name: "F-TS2",
-                blurb: "F-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "F-PE1",
-                blurb: "F-PE1 blurb",
-            },
-            {
-                name: "F-PE2",
-                blurb: "F-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "F-L1",
-                blurb: "F-L1 blurb",
-            },
-            {
-                name: "F-L2",
-                blurb: "F-L2 blurb",
-            },
-        ],
-    },
-    {
-        name: "Paris",
-        country: "France",
-        population: "11,276,701",
-        top_spots: [
-            {
-                name: "P-TS1",
-                blurb: "P-TS1 blurb",
-            },
-            {
-                name: "P-TS2",
-                blurb: "P-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "P-PE1",
-                blurb: "P-PE1 blurb",
-            },
-            {
-                name: "P-PE2",
-                blurb: "P-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "P-L1",
-                blurb: "P-L1 blurb",
-            },
-            {
-                name: "P-L2",
-                blurb: "P-L2 blurb",
-            },
-        ],
-    },
+  {
+    name: "Colmar",
+    country: "France",
+    population: "71,163",
+    top_spots: [
+      {
+        name: "F-TS1",
+        blurb: "F-TS1 blurb",
+      },
+      {
+        name: "F-TS2",
+        blurb: "F-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "F-PE1",
+        blurb: "F-PE1 blurb",
+      },
+      {
+        name: "F-PE2",
+        blurb: "F-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "F-L1",
+        blurb: "F-L1 blurb",
+      },
+      {
+        name: "F-L2",
+        blurb: "F-L2 blurb",
+      },
+    ],
+  },
+  {
+    name: "Paris",
+    country: "France",
+    population: "11,276,701",
+    top_spots: [
+      {
+        name: "P-TS1",
+        blurb: "P-TS1 blurb",
+      },
+      {
+        name: "P-TS2",
+        blurb: "P-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "P-PE1",
+        blurb: "P-PE1 blurb",
+      },
+      {
+        name: "P-PE2",
+        blurb: "P-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "P-L1",
+        blurb: "P-L1 blurb",
+      },
+      {
+        name: "P-L2",
+        blurb: "P-L2 blurb",
+      },
+    ],
+  },
 ];
 
 /**
  * To import: import { japanCities } from "./data.js";
  */
 export const japanCities = [
-    {
-        name: "Kyoto",
-        country: "Japan",
-        population: "1,459,640",
-        top_spots: [
-            {
-                name: "K-TS1",
-                blurb: "K-TS1 blurb",
-            },
-            {
-                name: "K-TS2",
-                blurb: "K-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "K-PE1",
-                blurb: "K-PE1 blurb",
-            },
-            {
-                name: "K-PE2",
-                blurb: "K-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "K-L1",
-                blurb: "K-L1 blurb",
-            },
-            {
-                name: "K-L2",
-                blurb: "K-L2 blurb",
-            },
-        ],
-    },
-    {
-        name: "Tokyo",
-        country: "Japan",
-        population: "8,336,599",
-        top_spots: [
-            {
-                name: "T-TS1",
-                blurb: "T-TS1 blurb",
-            },
-            {
-                name: "T-TS2",
-                blurb: "T-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "T-PE1",
-                blurb: "T-PE1 blurb",
-            },
-            {
-                name: "T-PE2",
-                blurb: "T-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "T-L1",
-                blurb: "T-L1 blurb",
-            },
-            {
-                name: "T-L2",
-                blurb: "T-L2 blurb",
-            },
-        ],
-    },
+  {
+    name: "Kyoto",
+    country: "Japan",
+    population: "1,459,640",
+    top_spots: [
+      {
+        name: "Nijo (Nijojo) Castle",
+        blurb: `Nijo Castle (Nijojo) is a UNESCO World Heritage Site that showcases the architectural 
+                splendor of the Edo period, featuring beautiful gardens and stunning wooden structures. 
+                Visitors can stroll through its historic halls, admire intricate wall paintings, and 
+                experience the unique nightingale floors that chirp underfoot.`,
+      },
+      {
+        name: "The Kyoto Railway Museum",
+        blurb: `The Kyoto Railway Museum offers an immersive journey through Japan's rich 
+          railway history, showcasing an impressive collection of trains and interactive exhibits. 
+          Visitors can explore vintage locomotives, learn about the evolution of rail travel, 
+          and even experience a simulated train ride.`,
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "Fire Ramen Kyoto - Menbaka",
+        blurb: ` A unique dining experience that combines delicious ramen with a dramatic flair, 
+        as chefs ignite the broth for a spectacular presentation. Guests can enjoy rich, flavorful 
+        noodles while witnessing the captivating fire show, making it a must-visit spot for 
+        adventurous food lovers.`,
+      },
+      {
+        name: "Matsuzakagyu What's Kyoto Muromachi",
+        blurb: `Specializes in exquisite Matsuzaka beef, renowned for its exceptional 
+        marbling and flavor, providing a gourmet experience in 
+        the heart of Kyoto. Diners can savor a variety of dishes prepared with this premium 
+        beef, all within a sophisticated setting that highlights the essence of Japanese cuisine.`,
+      },
+    ],
+    lodging: [
+      {
+        name: "Sora Niwa Terrace Kyoto",
+        blurb: `A stylish hotel that combines modern comfort with 
+        traditional Japanese aesthetics, offering guests a serene retreat in the heart of the city.
+         With spacious rooms, a tranquil garden terrace, and easily accessible local attractions, it 
+         is ideal for both relaxation and cultural immersion.`,
+      },
+      {
+        name: "Hiiragiya Hotel",
+        blurb: `A renowned ryokan that embodies the elegance of Kyoto’s traditional 
+        hospitality, offering guests a unique blend of luxurious comfort and rich cultural heritage. 
+        With beautifully appointed rooms, exquisite kaiseki cuisine, and personalized service, it provides
+        an unforgettable experience in an environment reflective of the charm of old Japan.`,
+      },
+    ],
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    population: "8,336,599",
+    top_spots: [
+      {
+        name: "T-TS1",
+        blurb: "T-TS1 blurb",
+      },
+      {
+        name: "T-TS2",
+        blurb: "T-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "T-PE1",
+        blurb: "T-PE1 blurb",
+      },
+      {
+        name: "T-PE2",
+        blurb: "T-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "T-L1",
+        blurb: "T-L1 blurb",
+      },
+      {
+        name: "T-L2",
+        blurb: "T-L2 blurb",
+      },
+    ],
+  },
 ];
 
 /**
  * To import: import { moroccoCities } from "./data.js";
  */
 export const moroccoCities = [
-    {
-        name: "Casablanca",
-        country: "Morocco",
-        population: "3,950,408",
-        top_spots: [
-            {
-                name: "CM-TS1",
-                blurb: "CM-TS1 blurb",
-            },
-            {
-                name: "CM-TS2",
-                blurb: "CM-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "CM-PE1",
-                blurb: "CM-PE1 blurb",
-            },
-            {
-                name: "CM-PE2",
-                blurb: "CM-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "CM-L1",
-                blurb: "CM-L1 blurb",
-            },
-            {
-                name: "CM-L2",
-                blurb: "CM-L2 blurb",
-            },
-        ],
-    },
-    {
-        name: "Rabat",
-        country: "Morocco",
-        population: "1,989,197",
-        top_spots: [
-            {
-                name: "R-TS1",
-                blurb: "R-TS1 blurb",
-            },
-            {
-                name: "R-TS2",
-                blurb: "R-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "R-PE1",
-                blurb: "R-PE1 blurb",
-            },
-            {
-                name: "R-PE2",
-                blurb: "R-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "R-L1",
-                blurb: "R-L1 blurb",
-            },
-            {
-                name: "R-L2",
-                blurb: "R-L2 blurb",
-            },
-        ],
-    },
+  {
+    name: "Casablanca",
+    country: "Morocco",
+    population: "3,950,408",
+    top_spots: [
+      {
+        name: "CM-TS1",
+        blurb: "CM-TS1 blurb",
+      },
+      {
+        name: "CM-TS2",
+        blurb: "CM-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "CM-PE1",
+        blurb: "CM-PE1 blurb",
+      },
+      {
+        name: "CM-PE2",
+        blurb: "CM-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "CM-L1",
+        blurb: "CM-L1 blurb",
+      },
+      {
+        name: "CM-L2",
+        blurb: "CM-L2 blurb",
+      },
+    ],
+  },
+  {
+    name: "Rabat",
+    country: "Morocco",
+    population: "1,989,197",
+    top_spots: [
+      {
+        name: "R-TS1",
+        blurb: "R-TS1 blurb",
+      },
+      {
+        name: "R-TS2",
+        blurb: "R-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "R-PE1",
+        blurb: "R-PE1 blurb",
+      },
+      {
+        name: "R-PE2",
+        blurb: "R-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "R-L1",
+        blurb: "R-L1 blurb",
+      },
+      {
+        name: "R-L2",
+        blurb: "R-L2 blurb",
+      },
+    ],
+  },
 ];
 
 /**
  * To import: import { argentinaCities } from "./data.js";
  */
 export const usCities = [
-    {
-        name: "Los Angeles",
-        country: "US",
-        population: "3,795,936",
-        state: "California",
-        top_spots: [
-            {
-                name: "LA-TS1",
-                blurb: "LA-TS1 blurb",
-            },
-            {
-                name: "LA-TS2",
-                blurb: "LA-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "LA-PE1",
-                blurb: "LA-PE1 blurb",
-            },
-            {
-                name: "LA-PE2",
-                blurb: "LA-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "LA-L1",
-                blurb: "LA-L1 blurb",
-            },
-            {
-                name: "LA-L2",
-                blurb: "LA-L2 blurb",
-            },
-        ],
-    },
-    {
-        name: "New York",
-        country: "US",
-        population: "8,097,282",
-        state: "New York",
-        top_spots: [
-            {
-                name: "NY-TS1",
-                blurb: "NY-TS1 blurb",
-            },
-            {
-                name: "NY-TS2",
-                blurb: "NY-TS2 blurb",
-            },
-        ],
-        places_to_eat: [
-            {
-                name: "NY-PE1",
-                blurb: "NY-PE1 blurb",
-            },
-            {
-                name: "NY-PE2",
-                blurb: "NY-PE2 blurb",
-            },
-        ],
-        lodging: [
-            {
-                name: "NY-L1",
-                blurb: "NY-L1 blurb",
-            },
-            {
-                name: "NY-L2",
-                blurb: "NY-L2 blurb",
-            },
-        ],
-    },
+  {
+    name: "Los Angeles",
+    country: "US",
+    population: "3,795,936",
+    state: "California",
+    top_spots: [
+      {
+        name: "LA-TS1",
+        blurb: "LA-TS1 blurb",
+      },
+      {
+        name: "LA-TS2",
+        blurb: "LA-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "LA-PE1",
+        blurb: "LA-PE1 blurb",
+      },
+      {
+        name: "LA-PE2",
+        blurb: "LA-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "LA-L1",
+        blurb: "LA-L1 blurb",
+      },
+      {
+        name: "LA-L2",
+        blurb: "LA-L2 blurb",
+      },
+    ],
+  },
+  {
+    name: "New York",
+    country: "US",
+    population: "8,097,282",
+    state: "New York",
+    top_spots: [
+      {
+        name: "NY-TS1",
+        blurb: "NY-TS1 blurb",
+      },
+      {
+        name: "NY-TS2",
+        blurb: "NY-TS2 blurb",
+      },
+    ],
+    places_to_eat: [
+      {
+        name: "NY-PE1",
+        blurb: "NY-PE1 blurb",
+      },
+      {
+        name: "NY-PE2",
+        blurb: "NY-PE2 blurb",
+      },
+    ],
+    lodging: [
+      {
+        name: "NY-L1",
+        blurb: "NY-L1 blurb",
+      },
+      {
+        name: "NY-L2",
+        blurb: "NY-L2 blurb",
+      },
+    ],
+  },
 ];
-
