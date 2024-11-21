1e0d68b7eb54a7a6cfbb8490e0a7bb01180a0e0c
Trinity Sanaa Bissahoyo <tbissaho@tesla21.eecs.utk.edu>
2024-11-21 10:27:37 -0500

    reversed searchbar

--- src/js/search_bar.js
+++ src/js/search_bar.js
@@ -5,13 +5,13 @@ document.addEventListener("keyup", function () {
 
   items.forEach(function (item) {
     if (item.innerHTML.toLowerCase().includes(input)) {
-      item.style.display = "block"; // Show items that match the query
-      // item.classList.remove("hidden");
+      //item.style.display = "block"; // Show items that match the query
+      item.classList.remove("hidden");
       item.classList.add("match");
     } else {
-      item.style.display = "none"; // Hide items that don't match
-      // item.classList.remove("match");
-      // item.classList.add("hidden");
+      //item.style.display = "none"; // Hide items that don't match
+      item.classList.remove("match");
+      item.classList.add("hidden");
     }
   });
 
